83dbf33e3d101296b463e7fc55be689f15f5aabf
giuseppe sabato <giu_sabato@alice.it>
2014-04-24 12:08:32 +0200

    Signed-off-by: giuseppe sabato <giu_sabato@alice.it>

--- DodgeHoles/gen/it/unisa/dodgeholes/R.java
+++ DodgeHoles/gen/it/unisa/dodgeholes/R.java
@@ -982,6 +982,7 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int ico_sett=0x7f020058;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1154,6 +1155,9 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
+        public static final int label_level=0x7f0a000f;
+        public static final int label_register=0x7f0a0011;
+        public static final int label_setting=0x7f0a0010;
     }
     public static final class style {
         /** 
